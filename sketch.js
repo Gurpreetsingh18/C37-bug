9b34d28376d66c338e7101a01009ad48d6d4ab2d
Gurpreetsingh <navpritsingh13@gmail.com>
2020-05-06 18:54:13 +0530

    added forms and sprites

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
+var allPlayers;
+var car1,car2,car3,car4,cars;
 
 var database;
 
@@ -9,13 +11,21 @@ var form, player, game;
 
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth,displayHeight);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
+  
 }
 
 
 function draw(){
+  if(playerCount==4){
+    game.update(1);
+  }
+  if(gameState==1){
+    game.play();
+    
+  }
 }
